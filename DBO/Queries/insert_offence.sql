b01f32a3355483ad4ed9e062c1bb86eb0e4d08ef
OphDev <opheliah95@gmail.com>
2021-04-21 13:20:16 +0100

    [Query] SQL for Insert Offence

---
+++ DBO/Queries/insert_offence.sql
@@ -0,0 +1,15 @@
+USE VehicleDB;
+INSERT INTO Offence (Offence_ID, Offence_description, Offence_maxFine, Offence_maxPoints) VALUES
+(1, 'Speeding', 1000, 3),
+(2, 'Speeding on a motorway', 2500, 6),
+(3, 'Seat belt offence', 500, 0),
+(4, 'Illegal parking', 500, 0),
+(5, 'Drink driving', 10000, 11),
+(6, 'Driving without a licence', 10000, 0),
+(7, 'Driving without a licence', 10000, 0),
+(8, 'Traffic light offences', 1000, 3),
+(9, 'Cycling on pavement', 500, 0),
+(10, 'Failure to have control of vehicle', 1000, 3),
+(11, 'Dangerous driving', 1000, 11),
+(12, 'Careless driving', 5000, 6),
+(13, 'Dangerous cycling', 2500, 0);
