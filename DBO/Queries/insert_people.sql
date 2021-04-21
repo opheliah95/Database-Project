0739f1222dc33cec6a56a338f3c9e74a7e449055
OphDev <opheliah95@gmail.com>
2021-04-21 20:24:55 +0100

    [TestData] Changed names to more fictitious places

--- DBO/Queries/insert_people.sql
+++ DBO/Queries/insert_people.sql
@@ -2,11 +2,11 @@ USE VehicleDB;
 
 # inserting data into people column
 INSERT INTO People (People_ID, People_Name, People_Address, People_Licence) VALUES
-(1, 'James Smith', '23 Barnsdale Road, Leicester', 'SMITH92LDOFJJ829'),
-(2, 'Jennifer Allen', '46 Bramcote Drive, Nottingham', 'ALLEN88K23KLR9B3'),
-(3, 'John Myers', '323 Derby Road, Nottingham', 'MYERS99JDW8REWL3'),
-(4, 'James Smith', '26 Devonshire Avenue, Nottingham', 'SMITHR004JFS20TR'),
-(5, 'Terry Brown', '7 Clarke Rd, Nottingham', 'BROWND3PJJ39DLFG'),
-(6, 'Mary Adams', '38 Thurman St, Nottingham', 'ADAMSH9O3JRHH107'),
-(7, 'Neil Becker', '6 Fairfax Close, Nottingham', 'BECKE88UPR840F9R'),
-(8, 'Angela Smith', '30 Avenue Road, Grantham', 'SMITH222LE9FJ5DS');
+(1, 'James Smith', '23 Barnsdale Road, Anbuford', 'SMITH92LDOFJJ829'),
+(2, 'Jennifer Allen', '46 Bramcote Drive, Zlario', 'ALLEN88K23KLR9B3'),
+(3, 'John Myers', '323 Derby Road, Uzlens', 'MYERS99JDW8REWL3'),
+(4, 'James Smith', '26 Devonshire Avenue, Zluchester', 'SMITHR004JFS20TR'),
+(5, 'Terry Brown', '7 Clarke Rd, Geadrough', 'BROWND3PJJ39DLFG'),
+(6, 'Mary Adams', '38 Thurman St, Uzlens', 'ADAMSH9O3JRHH107'),
+(7, 'Neil Becker', '6 Fairfax Close, Zlario', 'BECKE88UPR840F9R'),
+(8, 'Angela Smith', '30 Avenue Road, Anbuford', 'SMITH222LE9FJ5DS');
