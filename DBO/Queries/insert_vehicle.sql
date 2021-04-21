a60d4ce547351169aa6ba38b84667eabcb87b3ca
OphDev <opheliah95@gmail.com>
2021-04-21 13:20:03 +0100

    [Query] SQL for Insert Vehicle

---
+++ DBO/Queries/insert_vehicle.sql
@@ -0,0 +1,11 @@
+use VehicleDB;
+INSERT INTO Vehicle (Vehicle_ID, Vehicle_type, Vehicle_colour, Vehicle_licence) VALUES
+(12, 'Ford Fiesta', 'Blue', 'LB15AJL'),
+(13, 'Ferrari 458', 'Red', 'MY64PRE'),
+(14, 'Vauxhall Astra', 'Silver', 'FD65WPQ'),
+(15, 'Honda Civic', 'Green', 'FJ17AUG'),
+(16, 'Toyota Prius', 'Silver', 'FP16KKE'),
+(17, 'Ford Mondeo', 'Black', 'FP66KLM'),
+(18, 'Ford Focus', 'White', 'DJ14SLE'),
+(20, 'Nissan Pulsar', 'Red', 'NY64KWD'),
+(21, 'Renault Scenic', 'Silver', 'BC16OEA');
