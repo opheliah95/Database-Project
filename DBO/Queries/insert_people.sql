54163cde8de5b33ddd0f31d5de797ffba3af9e14
OphDev <opheliah95@gmail.com>
2021-04-21 13:13:54 +0100

    [Query] Added query to insert test people data

---
+++ DBO/Queries/insert_people.sql
@@ -0,0 +1,12 @@
+USE VehicleDB;
+
+# inserting data into people column
+INSERT INTO People (People_ID, People_Name, People_Address, People_Licence) VALUES
+(1, 'James Smith', '23 Barnsdale Road, Leicester', 'SMITH92LDOFJJ829'),
+(2, 'Jennifer Allen', '46 Bramcote Drive, Nottingham', 'ALLEN88K23KLR9B3'),
+(3, 'John Myers', '323 Derby Road, Nottingham', 'MYERS99JDW8REWL3'),
+(4, 'James Smith', '26 Devonshire Avenue, Nottingham', 'SMITHR004JFS20TR'),
+(5, 'Terry Brown', '7 Clarke Rd, Nottingham', 'BROWND3PJJ39DLFG'),
+(6, 'Mary Adams', '38 Thurman St, Nottingham', 'ADAMSH9O3JRHH107'),
+(7, 'Neil Becker', '6 Fairfax Close, Nottingham', 'BECKE88UPR840F9R'),
+(8, 'Angela Smith', '30 Avenue Road, Grantham', 'SMITH222LE9FJ5DS');
