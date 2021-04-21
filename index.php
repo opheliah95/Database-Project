7705bf25fdfb3023b153f4a858d1aa6832b771dd
OphDev <opheliah95@gmail.com>
2021-04-21 20:44:34 +0100

    [LOGIN] Added HTML code for login

--- index.php
+++ index.php
@@ -4,11 +4,23 @@
 
 <!DOCTYPE html>
 <html>
-<link rel="stylesheet" href="CSS/style.css">
+<link rel="stylesheet" href="style/style.css">
 <head>
     <title>Vehicle Database Project</title>
 </head>
 <body>
+<form method = "post" action = "login.php">
+Username: &nbsp; <input type = "text" name = "name" placeholder = "username">
+<?php echo isset($name_error)? "*".$name_error : "";?><br>
+
+
+Password:&nbsp;&nbsp;&nbsp;
+<input type = "password" name = "password" placeholder = "password">
+<?php echo isset($password_error )? "*".$password_error : "";?><br><br><br>
+<input type = "submit" value = "Login">
+</form>
+
+<?php echo isset($login_error )? "*".$login_error : "";?>
 
 </body>
 </html>
