f2ea408e53ebd616a061211e3924924e4e80c71c
OphDev <opheliah95@gmail.com>
2021-04-21 13:25:37 +0100

    [Query] Test SQL for Insert Officers

---
+++ DBO/Queries/insert_officer.sql
@@ -0,0 +1,5 @@
+USE VehicleDB;
+INSERT INTO Officer(Officer_Username, Officer_Role, Officer_Password)
+VALUES 			 ("Regan", "user", "plod123"),
+       			 ("Carter", "user", "fuzz42"),
+                 	("haskins", "admin", "copper99");
