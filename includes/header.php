c5aca29bddf34aef8aea4d7d2afbff3741a56b99
OphDev <opheliah95@gmail.com>
2021-04-22 20:18:58 +0100

    [Refresh] Added function for refresh page if there is connection error

--- includes/header.php
+++ includes/header.php
@@ -6,6 +6,13 @@ function get_css_path()
     echo $css;
 }
 
+// this function will redirect user to homepage
+function refresh_to_homepage($time = 5){
+    $home = "../index.php";
+    echo "This page will refresh in " .$time. " seconds";
+    header("refresh:$time; url=$home");
+}
+
 ?>
 
 
@@ -45,6 +52,7 @@ if (isset($_SESSION["user"]) && isset($_SESSION["id"]) && isset($_SESSION["role"
  } else {
 	echo "<p class = 'notlog'>You are not logged in</p>";
 	echo "<br>";
+    refresh_to_homepage();
 }
 
 ?>
