dbe0b466a0848924eaf317c4da1d4762571af5bc
OphDev <opheliah95@gmail.com>
2021-04-23 14:39:29 +0100

    [AddItem] Created files for adding items to database

---
+++ includes/Actions/Add/process_add.php
@@ -0,0 +1,49 @@
+<?php
+// get include path
+$parent_folder = "Actions";
+$include_dir = explode($parent_folder, __DIR__)[0];
+
+?>
+
+<?php
+// add helper path
+include_once($include_dir."Helper/connection_handler.php");
+//include session handler
+include($include_dir."Helper/session_handler.php");
+
+// check session
+if (check_session())
+{
+	exit;
+}
+?>
+
+
+<?php
+// check whether user pressed add buttons or cancels or they just open the page by accident
+if(isset($_GET["people"])) {
+	header("location:add_people.php");
+}
+elseif(isset($_GET["vehicle"])) {
+	header("location:add_vehicle.php");
+}
+elseif(isset($_GET["report"])) {
+	echo "Welcome report";
+}
+elseif(isset($_GET["cancel"])) {
+	header("location:userpage.php");
+}
+else {
+	echo '<html>
+			<body>
+				<p> You just arrive on this page by accident! </p>
+				<p> You will be redirected to login page </p>
+			</body>
+		  </html>';
+
+	refresh_to_homepage();
+
+}
+
+
+?>
