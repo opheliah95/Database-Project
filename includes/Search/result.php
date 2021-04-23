c49324d090dc0fb8cda016570dbc552a0f1efd1b
OphDev <opheliah95@gmail.com>
2021-04-23 11:50:18 +0100

    [Result] Added function to query search result

---
+++ includes/Search/result.php
@@ -0,0 +1,54 @@
+<html>
+
+<?php
+include("../dbconnection.php");
+session_start();
+include("../header.php");
+?>
+
+<?php
+// if session variables are destroyed
+if (!isset($_SESSION["user"]) || !isset($_SESSION["id"])) {
+	header("location:../login.php");
+	exit;
+}
+?>
+
+<?php
+// if user search by driver
+if (isset($_GET["driver"])) {
+	include("people.php");
+}
+
+
+
+
+// if user search by  vehicle
+elseif(isset($_GET["vehicle"])) {
+	include("vehicle.php");
+}
+
+
+
+
+
+elseif(isset($_GET["add"])) {
+	header("location:../Edit/add_report.php");
+	exit;
+}
+
+
+
+
+
+else {
+	header("location:../login.php");
+	exit;
+}
+
+?>
+
+
+
+</body>
+</html>
