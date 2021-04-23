3b39093b9725d7e7ffbc52cc5ec4a4cbbd33119d
OphDev <opheliah95@gmail.com>
2021-04-23 15:22:13 +0100

    [Const] Added const path for user folder

--- includes/Helper/const_handler.php
+++ includes/Helper/const_handler.php
@@ -4,6 +4,8 @@
     define("SPACE", " ");
     define("BACKSLASH", "\\");
     define("HELPER_DIR", "Helper");
+    define("USER_FOLDER", "User");
+    define("USER_PAGE_PATH", "\userpage.php");
 
     $include = explode(HELPER_DIR, __DIR__)[0];
     define("INCLUDE_PATH", $include);
@@ -11,8 +13,10 @@
     // two important paths --Database and Header
     $DB_Path = INCLUDE_PATH."dbconnection.php";
     $Header_Path = INCLUDE_PATH."header.php";
+    $User_Path = INCLUDE_PATH.USER_FOLDER;
 
     define("DB_PATH", $DB_Path);
     define("HEADER_PATH", $Header_Path);
+    define("USER_DIR_PATH", $User_Path);
 
 ?>
